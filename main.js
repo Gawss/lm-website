e373cd1c28617d011730862d4eeb74479ae9f2a9
Luis M. Palacio <luispalacio1997@hotmail.com>
2020-04-15 09:09:40 +0200

    mobile is not supported

--- main.js
+++ main.js
@@ -5,20 +5,25 @@ const app = express();
 let SERVER_PORT = 1337;
 
 app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/public/resources'));
 
 const server = app.listen(process.env.PORT || SERVER_PORT, () => {
     console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env)
     console.log('Server On', __dirname)
 });
 
+
+app.use(express.static(__dirname + '/public/resources'));
 app.get('/', (req, res) => {    
     console.log('GET /');
-    res.sendFile(__dirname + '/public/main.html');
+    if(process.env.PORT != undefined){
+        res.sendFile(__dirname + '/public/inprogress.html');
+    }else{
+        res.sendFile(__dirname + '/public/main.html');
+    }
 });
 
 app.use(express.static(__dirname + '/Unity/space_invaders'));
 app.get('/space_invaders', (req, res) => {    
-    console.log('GET /');
+    console.log('GET /space_invaders');
     res.sendFile(__dirname + '/Unity/space_invaders/index.html');
 });

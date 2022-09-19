5367e5144b2fd3f897f4c23edbc6d686783e72c0
Luis M. Palacio <luispalacio1997@hotmail.com>
2022-09-19 23:19:09 +0200

    Updating Cors

--- main.js
+++ main.js
@@ -47,6 +47,14 @@ app.get('/lm', (req, res) => {
     res.sendFile(__dirname + '/public/site/index.html');
 });
 
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 app.use(express.static(__dirname + '/Unity/DTOWN_WebGL'));
 app.get('/dtown', (req, res) => {    
     res.sendFile(__dirname + '/Unity/DTOWN_WebGL/index.html');

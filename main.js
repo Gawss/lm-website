a6ee3727f763c3cfeb6d6fef57f68f0eef8353e4
Luis M. Palacio <luispalacio1997@hotmail.com>
2025-04-21 20:55:10 +0200

    legacy + unity-webgl

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 
 // const cors = require('cors');
 
@@ -10,56 +10,67 @@ const app = express();
 
 let SERVER_PORT = 1337;
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
 const server = app.listen(process.env.PORT || SERVER_PORT, () => {
-    console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env)
-    console.log('Server On', __dirname)
+  console.log(
+    "Express server listening on port %d in %s mode",
+    server.address().port,
+    app.settings.env
+  );
+  console.log("Server On", __dirname);
 });
 
-
-app.use(express.static(__dirname + '/public/resources'));
-app.use(express.static(__dirname + '/public'));
-app.get('/', (req, res) => {    
-    console.log('GET /');
-    if(process.env.PORT != undefined){
-        res.sendFile(__dirname + '/public/inprogress.html');
-    }else{
-        res.sendFile(__dirname + '/public/main.html');
-    }
+app.use(express.static(__dirname + "/public/resources"));
+app.use(express.static(__dirname + "/public"));
+app.get("/", (req, res) => {
+  console.log("GET /");
+  if (process.env.PORT != undefined) {
+    res.sendFile(__dirname + "/public/inprogress.html");
+  } else {
+    res.sendFile(__dirname + "/public/main.html");
+  }
 });
 
-app.use(express.static(__dirname + '/public/About'));
-app.get('/luis', (req, res) => {    
-    console.log('GET /luis');
-    res.sendFile(__dirname + '/public/About/about.html');
+app.use(express.static(__dirname + "/public/About"));
+app.get("/luis", (req, res) => {
+  console.log("GET /luis");
+  res.sendFile(__dirname + "/public/About/about.html");
 });
 
-app.use(express.static(__dirname + '/Unity/space_invaders'));
-app.get('/space_invaders', (req, res) => {    
-    console.log('GET /space_invaders');
-    res.sendFile(__dirname + '/Unity/space_invaders/index.html');
+app.use(express.static(__dirname + "/Unity/space_invaders"));
+app.get("/space_invaders", (req, res) => {
+  console.log("GET /space_invaders");
+  res.sendFile(__dirname + "/Unity/space_invaders/index.html");
 });
 
-app.use(express.static(__dirname + '/public/AR'));
-app.get('/AR', (req, res) => {    
-    console.log('GET /AR');
-    res.sendFile(__dirname + '/public/AR/AR.html');
+app.use(express.static(__dirname + "/public/AR"));
+app.get("/AR", (req, res) => {
+  console.log("GET /AR");
+  res.sendFile(__dirname + "/public/AR/AR.html");
 });
 
-app.use(express.static(__dirname + '/public/site/assets'));
-app.use(express.static(__dirname + '/public/site'));
-app.get('/lm', (req, res) => {    
-    res.sendFile(__dirname + '/public/site/index.html');
+app.use(express.static(__dirname + "/public/site/assets"));
+app.use(express.static(__dirname + "/public/site"));
+app.get("/lm", (req, res) => {
+  res.sendFile(__dirname + "/public/site/index.html");
 });
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
-  });
-app.use(express.static(__dirname + '/Unity/DTOWN_WebGL'));
-app.get('/dtown', (req, res) => {
-    res.sendFile(__dirname + '/Unity/DTOWN_WebGL/index.html');
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
+app.use(express.static(__dirname + "/Unity/DTOWN_WebGL"));
+app.get("/dtown", (req, res) => {
+  res.sendFile(__dirname + "/Unity/DTOWN_WebGL/index.html");
+});
+
+app.use(express.static(__dirname + "/docs"));
+app.get("/portfolio", (req, res) => {
+  res.sendFile(__dirname + "/docs/index.html");
 });
